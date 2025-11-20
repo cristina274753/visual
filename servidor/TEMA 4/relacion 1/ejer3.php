43c6360b46625b870f43e70dc7dc36b041694cde
cristina274753 <paratodo2747@gmail.com>
2025-11-20 15:38:57 +0100

    tt

--- servidor/TEMA 4/relacion 1/ejer3.php
+++ servidor/TEMA 4/relacion 1/ejer3.php
@@ -1,52 +1,68 @@
 <?php
 
-    $nombre="";
+$nombre_usuario = '';
+
+if (isset($_COOKIE['nombre_usuario'])) {
+    $nombre_usuario = sanitizarEntrada($_COOKIE['nombre_usuario']??'');
+}
 
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre_usuario = sanitizarEntrada($_POST['nombre'] ?? '');
 
-    if($_SERVER['REQUEST_METHOD']==="POST" ) {
+    if ($nombre_usuario != '') {
 
-        $nombre=$_POST["nombre"];
+        setcookie('nombre_usuario', $nombre_usuario, time() + (5 * 60), "/");
 
-        setcookie("nombre", "$nombre", time()+ (15*60));
+        // Redirigimos para evitar el reenvío del formulario
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit();
     }
+}
 
-
+// Función para sanitizar entradas
+function sanitizarEntrada($data)
+{
+    // Eliminar espacios en blanco al inicio y al final
+    $data = trim($data);
+    // Eliminar barras invertidas
+    $data = stripslashes($data);
+    // Convertir caracteres especiales a entidades HTML
+    $data = htmlspecialchars($data);
+    return $data;
+}
 ?>
-
+<?php
+//código php
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang='es'>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        form{
-            display: 
-                <?php
-                    if (isset ($_COOKIE["nombre"])) {
-                        echo "none";
-                    }
-                ?>
-        }
-    </style>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Uso de cookies</title>
+    <link rel='stylesheet' href='https://cdn.simplecss.org/simple.min.css'>
 </head>
+
 <body>
-
-    <?php
-
-        if (isset ($_COOKIE["nombre"])){
-                $nombre=$_COOKIE["nombre"];
-                echo "hola $nombre";
-        }
-
-    ?>
-
-    <form action="" method="POST">
-        <p>indica tu nombre: </p>
-        <input type="text" id="nombre" name="nombre"  />
-        <input type="submit">	
-
-    </form>
+    <header>
+        <h2>Almacenamiento de cookie de usuario</h2>
+    </header>
+    <main>
+        <?php if ($nombre_usuario != ''):
+            echo "<p class='notice'>¡Bienvenido, $nombre_usuario !</p>";
+        else : ?>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <label for="nombre">Ingresa tu nombre:</label>
+                <input type="text" id="nombre" name="nombre" required>
+                <input type="submit" value="Enviar">
+            </form>
+        <?php endif; ?>
+    </main>
+    <footer>
+        <p>P.Lluyot</p>
+    </footer>
 </body>
+
 </html>
